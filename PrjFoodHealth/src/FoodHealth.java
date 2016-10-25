d344fa2b45afae47c999e01c4e6597614a37ac5a
Luis Casseres <luiscasseres@gmail.com>
2016-10-24 23:37:14 -0200

    Commit PrjFoodHealth

---
+++ PrjFoodHealth/src/FoodHealth.java
@@ -0,0 +1,96 @@
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+/*
+ * .
+ * Projeto Food&Health
+ * Versão 1.0
+ * 21-Out-2016
+ */
+/**
+ *
+ * @author 740600935
+ */
+public class FoodHealth extends javax.swing.JFrame {
+
+    public FoodHealth() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jRadioButtonMenuItem1 = new javax.swing.JRadioButtonMenuItem();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu2 = new javax.swing.JMenu();
+        jMenu3 = new javax.swing.JMenu();
+        jMenu5 = new javax.swing.JMenu();
+        jMenu6 = new javax.swing.JMenu();
+        jMenu7 = new javax.swing.JMenu();
+
+        jMenu1.setText("jMenu1");
+
+        jMenuItem1.setText("jMenuItem1");
+
+        jRadioButtonMenuItem1.setSelected(true);
+        jRadioButtonMenuItem1.setText("jRadioButtonMenuItem1");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setName("frmFoodHealth"); // NOI18N
+
+        jMenu2.setLabel("Vendas");
+        jMenuBar1.add(jMenu2);
+
+        jMenu3.setLabel("Pedidos");
+        jMenuBar1.add(jMenu3);
+
+        jMenu5.setLabel("Estoque");
+        jMenuBar1.add(jMenu5);
+
+        jMenu6.setLabel("Fornecedores");
+        jMenuBar1.add(jMenu6);
+
+        jMenu7.setLabel("Relatórios");
+        jMenuBar1.add(jMenu7);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 762, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 399, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FoodHealth().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu5;
+    private javax.swing.JMenu jMenu6;
+    private javax.swing.JMenu jMenu7;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JRadioButtonMenuItem jRadioButtonMenuItem1;
+    // End of variables declaration//GEN-END:variables
+}
